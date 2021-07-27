12ca32b11d018d024d0da2feb15ce9722e6864c4
Rajesh Lakkakula <rajeshlakkakula536@gmail.com>
2021-07-27 11:27:48 +0530

    This commit contains Movies App Assignment

--- app/src/main/java/com/rl/moviesapp/MainActivity.java
+++ app/src/main/java/com/rl/moviesapp/MainActivity.java
@@ -4,7 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+
+/**
+ * Create a RecyclerView Showing Movies with
+ *  Movie Name
+ *  Movie Image
+ *  Movie Ratings
+ *  Movie Crew
+ *
+ *  Use Retrofit to get the data from Intenet
+ *  Use Picasso/Glide to load image
+ *  Use MVVM Architecture , LiveData , DataBinding
+ *  Use Kotlin Programming to create Recyclerview Adapter (Only)
+ *
+ *
+ * */
+
+
 public class MainActivity extends AppCompatActivity {
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ app/src/main/java/com/rl/utils/Common.java
@@ -0,0 +1,10 @@
+package com.rl.utils;
+
+
+
+public class Common {
+
+    public static String URL = "https://rajtechi.github.io/AllShops/Movies/movies.json";
+
+
+}
